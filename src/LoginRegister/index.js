b90cee8380a8312d84021269768a20495786e6be
agraham <agraham0925@gmail.com>
2018-05-31 09:21:48 -0500

    can successfully add new user to firebase through react

--- src/LoginRegister/index.js
+++ src/LoginRegister/index.js
@@ -12,6 +12,7 @@ class LoginRegister extends Component {
 		}
 		this.login = this.login.bind(this);
           this.handleChange = this.handleChange.bind(this);
+          this.register = this.register.bind(this);
   // 		this.logout = this.logout.bind(this);
 	}
      handleChange(e) {
@@ -19,11 +20,18 @@ class LoginRegister extends Component {
      }
      login(e) {
           e.preventDefault();
-          console.log("you are trying to log in")
+          // console.log("you are trying to log in")
           firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {  
           }).catch((err) => {
                console.log(err)
           });
+     }
+     register(e){
+          e.preventDefault();
+          firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+          .catch((err) => {
+               console.log(err);
+          })
      }
 
  // {this.state.user ?
@@ -35,10 +43,17 @@ class LoginRegister extends Component {
 	render() {
 		return(
 			<div>
+                    <h2>Login</h2>
 				<form>
-                         <input type="text" name="email" placeholder="username" value={this.state.email} onChange={this.handleChange} /> <br />
+                         <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} /> <br />
                          <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} /> <br />
                          <input type="submit" onClick={this.login}/>
+                    </form>
+                    <h2>Register</h2>
+                    <form>
+                         <input type="text" name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} /> <br />
+                         <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} /> <br />
+                         <button onClick={this.register}> Register </button>
                     </form>
 			</div>
 		)
