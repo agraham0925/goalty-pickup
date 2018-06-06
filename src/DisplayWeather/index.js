055255ed49d132dbca6b99fb7c1946e637f8cbd1
agraham <agraham0925@gmail.com>
2018-06-06 15:35:44 -0500

    added comments to weatherDisplay methods

--- src/DisplayWeather/index.js
+++ src/DisplayWeather/index.js
@@ -10,13 +10,13 @@ const DisplayWeather = ({weatherForecast}) => {
 		//takes date from API and changes to readable format with timestamp
 		const upDate = new Date(theDay)
 
+		//returns day of the week with 0-6 (sun-sat)
 		const findDay = upDate.getDay();
-
-		console.log(findDay, " textDay values")
 
 		//removes the timestamp
 		const textDate = upDate.toDateString()
 
+		//returning only information for Sunday and Saturday
 		if(findDay === 0 || findDay === 6) {
 			return (
 				<div>
